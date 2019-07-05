a0c7ac7a8f5f966c97b5b0c7f87b67336ced23ff
declercq sebastien <sdq@afnor.fr>
2019-07-05 15:15:51 +0200

    Starting i18n transition

--- PurBeurre/settings.py
+++ PurBeurre/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 
 import os
 import django_heroku
+from django.utils.translation import gettext_lazy as _
+
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -114,6 +116,8 @@ AUTH_PASSWORD_VALIDATORS = [
 # https://docs.djangoproject.com/en/2.1/topics/i18n/
 
 LANGUAGE_CODE = 'en-us'
+
+LANGUAGES = (('en', _('English')), ('fr', _('Français')))
 
 TIME_ZONE = 'UTC'
 
